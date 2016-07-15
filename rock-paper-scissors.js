fdb4eebc277003dbe52fb0f166a26d3bc0aa39e5
Christopher McCloud <mccloud.christopher@gmail.com>
2016-07-15 08:49:34 -0300

    New Problem: Rock, Paper, Scissors

---
+++ rock-paper-scissors.js
@@ -0,0 +1,17 @@
+// Create a function, rps, which returns a 2 dimensional array of all the possible outcomes of a three round game of
+// rock, paper, scissors. E.G.
+// rps() => [['rock','rock','rock'], ['rock','rock','paper'], ['rock','rock','scissors']....]
+
+var rps = function () {
+
+};
+
+// Bonus: Create a function, rpsN, which returns a 2 dimensional array of all the possible outcomes of an n-round game
+// of rock, paper, scissors, E.G.
+// rpsN(0) => [[]]
+// rpsN(1) => [['rock'], ['paper'], ['scissors']]
+// rpsN(2) => [['rock', 'rock'], ['rock', 'paper'], ['rock', 'scissors'], ['paper', 'rock']....]
+
+var rpsN = function (rounds) {
+
+};
