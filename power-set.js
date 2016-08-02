ca88e80c22b73c18f218fcf23e4d6ae164628455
Paul McCloud <paul.mccloud@gmail.com>
2016-08-02 17:07:51 -0300

    creates powerset and robotpaths answers

--- power-set.js
+++ power-set.js
@@ -13,9 +13,100 @@
  * Example 2 :
  *
  * powerSet("jump")
- * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
+ * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", 
+ "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
-var powerSet = function (string) {
+ var powerSetReduce = function(string) {
+ 	var string = removeDuplicates(string);
+ 	var string = string.split('');
+ 	return string.reduce(function(result, char) {
+ 		return result.concat(result.map(function (ele) {
+ 			return order(ele+char);
 
-};
+ 		}))
+ 	}, ['']);
+	function removeDuplicates (string) {
+		var result = '';
+		for (var i = 0; i < string.length; i ++) {
+			if (result.indexOf(string[i]) === -1) {
+				result = result + string[i];
+			}
+		}
+		return result;
+	}
+	function order (string) {
+		return string.split("").sort().reduce(function(total, char) {
+			return total+char;
+		}, '');
+	}
+ }
+
+ console.log(powerSetReduce('jump'));
+
+var powerSet = function (string) {
+	var string = removeDuplicates(string);
+	var result = [""];
+	for (var i = 0; i < string.length; i++) {
+		result = getComb(result, string[i]);
+	}
+	function getComb (result, char) {
+		var newResult = [];
+		result.forEach(function(ele) {
+			var entry = orderStringArray(ele+char);
+			newResult.push(entry);
+		})
+		return result.concat(newResult);
+	}
+	function removeDuplicates (string) {
+		var result = '';
+		for (var i = 0; i < string.length; i ++) {
+			if (result.indexOf(string[i]) === -1) {
+				result = result + string[i];
+			}
+		}
+		return result;
+	}
+	function orderStringArray (string) {
+		var result = [];
+		var newString = '';
+		for (var i = 0; i < string.length; i ++) {
+			result.push(string[i]);
+		}
+		result.sort();
+		result.forEach(function(char) {
+			newString = newString + char;
+		});
+		return newString;
+	}
+	return result;
+}
+
+console.log(powerSet('jump'));
+console.log(powerSet('juump'));
+
+
+function orderString (string) {
+	var result = string;
+	for (var i = 1; i < string.length; i ++) {
+		if (string[i] < string[i-1]) {
+			result = switchAtIndex(result, i);
+			result = orderString(result);
+		}
+	}
+	return result;
+}
+
+function switchAtIndex(string, index) {
+	var newString = '';
+	if (index === 1) {
+		newString = string[1] + string[0] + string.slice(2);
+	}
+	else if (index === string.length-1) {
+		newString = string.slice(0,index-1) + string[index] + string[index-1];
+	}
+	else {
+		newString = string.slice(0,index-1) + string[index] + string[index-1] + string.slice(index+1);
+	}
+	return newString;
+}
--- robot-paths.js
+++ robot-paths.js
@@ -9,6 +9,62 @@
  *
  */
 
-var robotPaths = function () {
-
+var robotPaths = function (x, y) {
+	var grid = createGrid(x, y);
+	console.log(grid);
+	var pathsArray = [];
+	var positionArray = [];
+	var start = [0,0];
+	var target = [x-1, y-1];
+	traverseGrid(start, positionArray);
+	console.log(pathsArray);
+	return pathsArray.length;
+	function createGrid(x,y) {
+		var grid = [];
+		for (var i = 0; i < x; i++) {
+			var row = [];
+			for (var j = 0; j < y; j++) {
+				row.push([i,j]); 
+			}
+			grid.push(row);
+		}
+		return grid;
+	}
+	function traverseGrid(point, positionArray) {
+		potentialMoves = getMoves(point, grid);
+		potentialMoves.forEach(function(newPoint) {
+			var newPositionArray = positionArray.slice();
+			newPositionArray.push(point.toString());
+			if (newPoint[0] === target[0] && newPoint[1] === target[1]) {
+				pathsArray.push(newPositionArray);
+			}
+			else if (positionArray.indexOf(newPoint.toString()) === -1) {
+				traverseGrid(newPoint, newPositionArray);
+			}
+		});
+		function getMoves(point, grid) {
+			var moves = [];
+			var x = point[0];
+			var y = point[1];
+			var xLim = grid[0].length;
+			var yLim = grid.length;
+			if (x + 1 < xLim) {
+				moves.push([x+1, y]);
+			}
+			if (x - 1 >= 0) {
+				moves.push([x-1, y]);
+			}
+			if (y + 1 < yLim) {
+				moves.push([x, y+1]);
+			}
+			if (y -1 >= 0) {
+				moves.push([x, y-1]);
+			}
+			return moves;
+		}
+	};
 };
+
+console.log(robotPaths(3,3));
+
+
